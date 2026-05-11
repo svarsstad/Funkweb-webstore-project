87e45f2d3452e187014bb34fdef48fa2247c3248
svarsstad <sandsvarstad@gmail.com>
2026-05-11 14:40:08 +0200

    Done User Model
    Untested, need the user page on dash board to test. anything not found/ not matching db will be left empty when program runs.

--- Project Backend/Models/User.cs
+++ Project Backend/Models/User.cs
@@ -1,11 +1,33 @@
-﻿namespace Project_Backend.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Project_Backend.Models
 {
+    [BsonIgnoreExtraElements]
     public class User
     {
-        /*What goes here: This is where you define the "shape" of your data using plain C# classes.
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
 
-Examples: User.cs, Product.cs, Order.cs.
+        [BsonElement("displayName")]
+        public string DisplayName { get; set; } = string.Empty;
 
-        */
+        [BsonElement("email")]
+        public string Email { get; set; } = string.Empty;
+
+        [BsonElement("password")]
+        public string Password { get; set; } = string.Empty;
+
+        // List of Order document ids
+        [BsonElement("orders")]
+        public List<string> Orders { get; set; } = new();
+
+        // List of Product document ids
+        [BsonElement("favoriteProductIds")]
+        public List<string> FavoriteProductIds { get; set; } = new();
+
+        [BsonElement("lastLogin")]
+        public DateTime LastLogin { get; set; }
     }
 }
