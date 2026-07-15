e85540f101bd3e47a37c1da18903c25944132211
svarsstad <sandsvarstad@gmail.com>
2026-07-15 14:07:11 +0200

    dashboard add order fixed user and product list
    todo: add some fields for amount to order, a list of added products, and a save/clear order button

--- Project Backend/Models/Product.cs
+++ Project Backend/Models/Product.cs
@@ -9,7 +9,7 @@ namespace Project_Backend.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string? Id { get; set; }
+        public string Id { get; set; } = string.Empty;
 
         // Put the EXACT spelling/capitalization from MongoDB inside the quotes here
         [JsonPropertyName("productName")]
